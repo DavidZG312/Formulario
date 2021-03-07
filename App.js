75df8ee790be04ff657921bff5a1c843fe48132e
David <lsix1202@hotmail.com>
2021-03-07 18:24:45 -0500

    Mi primer formulario

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, {Fragment, useState} from 'react'
+import Tarea from './Components/Tarea';
+
+function App() {
+
+
+
+  return (
+    <>
+    <Tarea />
+    </>
+    
+  );
+}
+
+export default App;
